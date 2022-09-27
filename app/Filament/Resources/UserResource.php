1a7d1b247cb60aa932eacaa316863fd38ba05c21
Mark Kimsal <metrofindings@gmail.com>
2022-09-27 18:42:56 -0400

    ft: add tags

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Filament\Resources\UserResource\RelationManagers;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\SpatieTagsInput;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema(static::getFormSchema(Forms\Components\Card::class))
+            ->columns([
+                'sm' => 3,
+                'lg' => null,
+            ]);
+    }
+
+    public static function getFormSchema(string $layout = Forms\Components\Grid::class): array
+    {
+        return [
+            Forms\Components\Group::make()
+                ->schema([
+                    $layout::make()
+                        ->schema([
+                            Forms\Components\Grid::make(2)
+                            ->schema([
+                                Section::make('Tags')
+                                ->schema([
+                                    // bug in livewire with visibility toggles
+                                    SpatieTagsInput::make('subject')
+                                        ->name("Subject")
+                                        ->type('subject'),
+                                ]),
+                            ])
+                        ])
+                ])
+        ];
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }    
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Tags\HasTags;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+    use HasTags;
 
     /**
      * The attributes that are mass assignable.
